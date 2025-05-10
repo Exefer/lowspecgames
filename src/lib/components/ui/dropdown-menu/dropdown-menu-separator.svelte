ec156736c5d5a2734cf6d543cdd03dd19e306074
Exefer <jessieodeh08@gmail.com>
2025-05-10 09:38:43 +0200

    fix: make dropdown separator visible in dark mode

--- src/lib/components/ui/dropdown-menu/dropdown-menu-separator.svelte
+++ src/lib/components/ui/dropdown-menu/dropdown-menu-separator.svelte
@@ -11,6 +11,6 @@
 
 <DropdownMenuPrimitive.Separator
   bind:ref
-  class={cn("-mx-1 my-1 h-px bg-muted", className)}
+  class={cn("-mx-1 my-1 h-px bg-muted dark:bg-accent", className)}
   {...restProps}
 />
